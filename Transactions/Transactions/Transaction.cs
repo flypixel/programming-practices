3c5780bbc38c780d27c8868fee51fda3d0e32b14
GLOBALLOGIC / VOLODYMYR KORENKOV <volodymyr.korenkov@globallogic.com>
2019-04-22 20:48:42 +0300

    transactions implementation

---
+++ Transactions/Transactions/Transaction.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Transactions
+{
+    public class Transaction<TKey, TValue> where TValue: class
+    {
+        private static object _locker = new object();
+
+        private Dictionary<TKey, TValue> _addBuffer = new Dictionary<TKey, TValue>();
+
+        private HashSet<TKey> _removeBuffer = new HashSet<TKey>();
+
+        public Guid Id { get; } = Guid.NewGuid();
+
+        private Transaction()
+        {
+        }
+
+        public static Transaction<TKey, TValue> Create()
+        {
+            Monitor.Enter(_locker);
+            var trans = new Transaction<TKey, TValue>();
+            return trans;
+        }
+
+        public bool Add(TKey key, TValue value)
+        {
+            if (_addBuffer.ContainsKey(key))
+            {
+                return false;
+            }
+            _addBuffer.Add(key, value);
+            _removeBuffer.Remove(key);
+            return true;
+        }
+
+        internal TValue Get(TKey key)
+        {
+            _addBuffer.TryGetValue(key, out var value);
+            return value;
+        }
+
+        internal bool Remove(TKey key)
+        {
+            if (_addBuffer.Remove(key))
+            {
+                return true;
+            }
+
+            return _removeBuffer.Add(key);
+        }
+
+        internal IReadOnlyDictionary<TKey, TValue> GetBuffer()
+        {
+            return _addBuffer;
+        }
+
+
+        internal void Release()
+        {
+            Monitor.Exit(_locker);
+        }
+    }
+}
---
+++ Transactions/Transactions/TransactionHashMap.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Transactions
+{
+    public class TransactionHashMap<TKey, TValue> where TValue: class
+    {
+        private Dictionary<TKey, TValue> _inner = new Dictionary<TKey, TValue>();
+        private Stack<Transaction<TKey, TValue>> _transactions = new Stack<Transaction<TKey, TValue>>();
+
+        public bool Add(TKey key, TValue value)
+        {
+            var trans = _transactions.Peek();
+            if (_inner.ContainsKey(key))
+            {
+                return false;
+            }
+
+            return trans.Add(key, value);
+        }
+
+        public TValue Get(TKey key)
+        {
+            _inner.TryGetValue(key, out var value);
+            if (value is null)
+            {
+                return _transactions.Peek().Get(key);
+            }
+            return value;
+        }
+
+        public bool Remove(TKey key)
+        {
+            return _transactions.Peek().Remove(key);
+        }
+
+        public Transaction<TKey, TValue> BeginTransaction()
+        {
+            var trans = Transaction<TKey, TValue>.Create();
+            _transactions.Push(trans);
+            return trans;
+        }
+
+        public bool Commit(Transaction<TKey, TValue> trans)
+        {
+            foreach (var kvp in trans.GetBuffer())
+            {
+                if (_inner.ContainsKey(kvp.Key))
+                {
+                    Release(trans);
+                    return false;
+                }
+            }
+
+            foreach (var kvp in trans.GetBuffer())
+            {
+                _inner.Add(kvp.Key, kvp.Value);
+            }
+
+            Release(trans);
+            return true;
+        }
+
+        public void RollBack(Transaction<TKey, TValue> trans)
+        {
+            Release(trans);
+        }
+
+        private void Release(Transaction<TKey, TValue> trans)
+        {
+            var release = _transactions.Pop();
+            trans.Release();
+
+            if (release.Id.Equals(trans.Id))
+            {
+                throw new ApplicationException("Deadlock expected");
+            }
+        }
+    }
+}
