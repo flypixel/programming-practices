41e92b640c45523480d5607eb720c3d054217458
flypixel <pixbeaver@gmail.com>
2019-06-08 00:41:50 +0300

    Update LinkedListNode.cs

--- LinkedList/LinkedList/LinkedListNode.cs
+++ LinkedList/LinkedList/LinkedListNode.cs
@@ -6,7 +6,7 @@
 
         public LinkedListNode<T> Next { get; set; }
 
-        public LinkedListNode(T value, LinkedListNode<T> next)
+        public LinkedListNode(T value, LinkedListNode<T> next = null)
         {
             Value = value;
             Next = next;
