58d74322b44d2be4abda85c9356e7d797f5726d2
GLOBALLOGIC / VOLODYMYR KORENKOV <volodymyr.korenkov@globallogic.com>
2019-06-07 14:13:50 +0300

    revert

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -34,12 +34,12 @@ namespace LinkedList
         // lock free mutation
         public void Push(T value)
         {
-            LinkedListNode<T> prev = null;
+            LinkedListNode<T> prev;
             LinkedListNode<T> created;
 
             do
             {
-                Interlocked.Exchange(ref prev, _head);
+                prev = _head;
                 created = new LinkedListNode<T>(value, _head);
                 Interlocked.CompareExchange(ref _head, created, prev);
             } while (_head.Equals(prev));
